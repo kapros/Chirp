6cd293d80332f2029b93519047b853f58a682608
kapros <kamil.prosowski@gmail.com>
2020-12-31 18:58:16 +0100

    move IdentityController from V1 to general
    Considering that from the start the controller was not meant to be versioned, it's only appropriate

--- Chirp/Controllers/IdentityController.cs
+++ Chirp/Controllers/IdentityController.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chirp.Commands;
 using Chirp.Contracts.V1;
 using Chirp.Contracts.V1.Requests;
 using Chirp.Contracts.V1.Responses;
 using Chirp.Services;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Chirp.Controllers.V1
+namespace Chirp.Controllers
 {
     [Route("api")]
     [ApiController]
     public class IdentityController : ControllerBase
     {
         private readonly IIdentityService _identityService;
+        private readonly IMediator _mediator;
 
-        public IdentityController(IIdentityService identityService)
+        public IdentityController(IIdentityService identityService, IMediator mediator)
         {
             _identityService = identityService;
+            _mediator = mediator;
         }
 
         [HttpPost(ApiRoutes.Identity.Register)]
@@ -48,8 +52,8 @@ namespace Chirp.Controllers.V1
             if (!authResponse.Success)
                 return BadRequest(new AuthFailedResponse { Errors = authResponse.Errors });
 
-            return Ok(new AuthSuccessResponse 
-            { 
+            return Ok(new AuthSuccessResponse
+            {
                 Token = authResponse.Token,
                 RefreshToken = authResponse.RefreshToken
             });
@@ -68,6 +72,14 @@ namespace Chirp.Controllers.V1
                 Token = authResponse.Token,
                 RefreshToken = authResponse.RefreshToken
             });
+        }
+
+        [HttpDelete(ApiRoutes.Identity.Delete)]
+        public async Task<IActionResult> Delete([FromRoute] Guid userId)
+        {
+            var deleteCommand = DeleteUserCommand.FromGuid(userId);
+            await _mediator.Publish(deleteCommand);
+            return Ok();
         }
     }
 }
