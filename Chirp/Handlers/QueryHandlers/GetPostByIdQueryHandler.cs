e99cd6a583882465b6b2ca7598cb50c9d110e576
kapros <kamil.prosowski@gmail.com>
2020-12-31 19:05:30 +0100

    add query and query handler for GetPostById

---
+++ Chirp/Handlers/QueryHandlers/GetPostByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Chirp.Contracts.V1.Responses;
+using Chirp.Queries;
+using Chirp.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chirp.Handlers.QueryHandlers
+{
+    public class GetPostByIdQueryHandler : IRequestHandler<GetPostByIdQuery, PostResponse>
+    {
+        private readonly IPostService _postService;
+        private readonly IMapper _mapper;
+
+        public GetPostByIdQueryHandler(IPostService postService, IMapper mapper)
+        {
+            _postService = postService;
+            _mapper = mapper;
+        }
+
+        public async Task<PostResponse> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
+        {
+            var post = await _postService.GetPostByIdAsync(request.PostId);
+
+            return post != null ? _mapper.Map<PostResponse>(post) : null;
+        }
+    }
+}
---
+++ Chirp/Queries/GetPostByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Chirp.Contracts.V1.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Queries
+{
+    public class GetPostByIdQuery : IRequest<PostResponse>
+    {
+        public Guid PostId { get; set; }
+    }
+}
