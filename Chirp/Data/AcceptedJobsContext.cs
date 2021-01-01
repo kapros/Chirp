ca41abf403919fbaefa742f2589a6f7947907b01
kapros <kamil.prosowski@gmail.com>
2021-01-01 20:40:51 +0100

    add accepted jobs service and db

---
+++ Chirp/Data/AcceptedJobsContext.cs
@@ -0,0 +1,20 @@
+﻿using Chirp.Contracts.V1.Responses;
+using Chirp.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Data
+{
+    public class AcceptedJobsContext : DbContext
+    {
+        public AcceptedJobsContext(DbContextOptions<AcceptedJobsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AcceptedJob> Jobs { get; set; }
+    }
+}
---
+++ Chirp/Services/IAcceptedService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public interface IAcceptedService
+    {
+        Task<object> GetIdOfCompletedJob(string id);
+
+        Task<bool> IsJobFinished(string id);
+    }
+}
