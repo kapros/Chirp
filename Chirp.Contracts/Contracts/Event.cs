0c4182d575a090e9aa79c2cba4807f455ebacf87
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:57:24 +0100

    add event poco

---
+++ Chirp.Contracts/Contracts/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace Chirp.Contracts
+{
+    public class Event<T>
+    {
+        public Event() { }
+
+        public string Version { get; set; }
+
+        public string EventName { get; set; }
+
+        public T Message { get; set; }
+    }
+}
