450dff87c172fb0560ee3092e0dde8bc89016b4f
kapros <kamil.prosowski@gmail.com>
2021-01-01 15:02:50 +0100

    add DeleteUserCommand

---
+++ Chirp/Commands/DeleteUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Commands
+{
+    public class DeleteUserCommand : INotification
+    {
+        public string UserId { get; set; }
+
+        public static DeleteUserCommand FromGuid(Guid userId) => new DeleteUserCommand { UserId = userId.ToString() };
+    }
+}
