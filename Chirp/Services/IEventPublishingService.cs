6dc0a5ec5ca2be279f1fcec4175996c707c07a6a
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:51:02 +0100

    Create service to publish events

---
+++ Chirp/Services/IEventPublishingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Chirp.Contracts;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public interface IEventPublishingService
+    {
+        Task<bool> SendEvent<T>(Event<T> @event);
+
+        Task<bool> SendEvents<T>(IEnumerable<Event<T>> events);
+    }
+}
