491d6b01c4ea184a78be86f8523ce51381a7cee7
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:52:09 +0100

    create message queue service

---
+++ Chirp/Services/IMessageQueueService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public interface IMessageQueueService
+    {
+        Task<bool> Publish<T>(T message);
+
+        Task<bool> Publish(string raw);
+    }
+}
