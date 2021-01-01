120c1da5c81ebbb890564bc7c49d9c35f2e74bbd
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:56:42 +0100

    add implementation for events

---
+++ Chirp/Services/EventPublishingService.cs
@@ -0,0 +1,27 @@
+﻿using Chirp.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public class EventPublishingService : IEventPublishingService
+    {
+        private readonly IEventClient _eventClient;
+
+        public EventPublishingService(IEventClient eventClient)
+        {
+            _eventClient = eventClient;
+        }
+        public Task<bool> SendEvent<T>(Event<T> @event)
+        {
+            return _eventClient.PublishEvent<T>(@event);
+        }
+
+        public Task<bool> SendEvents<T>(IEnumerable<Event<T>> events)
+        {
+            return _eventClient.PublishEvents<T>(events);
+        }
+    }
+}
