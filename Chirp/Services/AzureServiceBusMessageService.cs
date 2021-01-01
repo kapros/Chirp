88eb830b8f6bd722ac6c2cae29061d2aed3a1ba4
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:55:56 +0100

    add message queue service for Azure Service Bus

---
+++ Chirp/Services/AzureServiceBusMessageService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public class AzureServiceBusMessageSender : IMessageQueueService
+    {
+        private readonly IQueueClient _queueClient;
+
+        public AzureServiceBusMessageSender(IQueueClient queueClient)
+        {
+            _queueClient = queueClient;
+        }
+
+        public async Task<bool> Publish<T>(T obj)
+        {
+            try
+            {
+                var serializedMessage = JsonConvert.SerializeObject(obj);
+                var message = new Message(Encoding.UTF8.GetBytes(serializedMessage));
+                await _queueClient.SendAsync(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging here
+                return false;
+            }
+        }
+
+        public async Task<bool> Publish(string raw)
+        {
+            var message = new Message(Encoding.UTF8.GetBytes(raw));
+
+            try
+            {
+                await _queueClient.SendAsync(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging here
+                return false;
+            }
+        }
+    }
+}
