457a1bfe3de6866b5206c29bcd2cfb1a869e9e93
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:59:58 +0100

    add queue settings for Azure Service Bus

---
+++ Chirp/Configuration/AzureServiceBusQueueSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Configuration
+{
+    public class AzureServiceBusQueueSettings
+    {
+        public string Address { get; set; }
+
+        public string Topic { get; set; }
+    }
+}
