eb1a31a62c36e25f0f9a14ed7bf8932dba50cb88
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:52:42 +0100

    add an event client

---
+++ Chirp/Services/IEventClient.cs
@@ -0,0 +1,15 @@
+﻿using Chirp.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public interface IEventClient
+    {
+        Task<bool> PublishEvent<TEvent>(Event<TEvent> @event);
+
+        Task<bool> PublishEvents<TEvent>(IEnumerable<Event<TEvent>> events);
+    }
+}
