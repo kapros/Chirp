c41c961f326ffb05bde88ea2450bb99061175732
kapros <kamil.prosowski@gmail.com>
2021-01-01 20:42:02 +0100

    add serialization service

---
+++ Chirp/Services/ISerializationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirp.Services
+{
+    public interface ISerializationService
+    {
+        static Encoding Encoding => Encoding.UTF8;
+
+        Task<string> Serialize<T>(T data);
+
+        Task<T> Deserialize<T>(string serialized);
+
+        Task<string> GetString(string obj);
+    }
+}
