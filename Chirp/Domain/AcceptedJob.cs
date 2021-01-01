9372d598b3265a51ed834403b242cf290a9a6b49
kapros <kamil.prosowski@gmail.com>
2021-01-01 20:36:32 +0100

    create a new domain object for eccapted jobs

---
+++ Chirp/Domain/AcceptedJob.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using ZeroFormatter;
+
+namespace Chirp.Domain
+{
+    public class AcceptedJob
+    {
+        public string JobId { get; set; }
+
+        [Column("Resource")]
+        public string Controller { get; set; }
+
+        public DateTime DateStarted { get; set; }
+
+        public DateTime? DateFinished { get; set; }
+
+        [Column("ResourceId")]
+        public object CreatedObjectId { get; set; }
+
+        public string Payload { get; set; }
+
+        public bool IsFinished() => DateFinished != null && CreatedObjectId != null;
+    }
+}
