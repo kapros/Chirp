927a64b55f4e21b983710b54ca294cacdf8ecf80
kapros <kamil.prosowski@gmail.com>
2021-01-01 14:59:34 +0100

    add installer for Azure Service Bus

---
+++ Chirp/Setup/AzureServiceBusQueueInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Chirp.Configuration;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Setup
+{
+    public class AzureServiceBusQueueInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var queueSettings = new AzureServiceBusQueueSettings();
+            configuration.GetSection("Queues:Queue").Bind(queueSettings);
+
+            var queueClient = new QueueClient(queueSettings.Address, queueSettings.Topic, retryPolicy: RetryPolicy.Default);
+
+            services.AddSingleton(queueClient);
+        }
+    }
+}
