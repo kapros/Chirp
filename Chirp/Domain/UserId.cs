9df8b64a2ed0a6dbb5db3c43614034388c09eff1
kapros <kamil.prosowski@gmail.com>
2021-01-02 22:46:35 +0100

    add new object to represent current user making the call

---
+++ Chirp/Domain/UserId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Domain
+{
+    public class UserId
+    {
+        public Guid Id { get; }
+
+        public UserId(Guid userId)
+        {
+            Id = userId;
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString();
+        }
+    }
+}
