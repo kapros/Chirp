38e928f46a1fd343bedd2780c9807a37f0d82df2
kapros <kamil.prosowski@gmail.com>
2021-01-02 20:29:57 +0100

    add query and handler for UserOwnsPost

---
+++ Chirp/Handlers/QueryHandlers/CheckUserOwnsPostQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Chirp.Queries;
+using Chirp.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chirp.Handlers.QueryHandlers
+{
+    public class CheckUserOwnsPostQueryHandler : IRequestHandler<UserOwnsPostQuery, (bool Success, bool PostFound, bool UserOwnsPost)>
+    {
+        private readonly IPostService _postService;
+
+        public CheckUserOwnsPostQueryHandler(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public async Task<(bool Success, bool PostFound, bool UserOwnsPost)> Handle(UserOwnsPostQuery request, CancellationToken cancellationToken)
+        {
+            var userOwnsPost = await _postService.UserOwnsPost(request.PostId, request.UserId);
+            return userOwnsPost;
+        }
+    }
+}
---
+++ Chirp/Queries/UserOwnsPostQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Queries
+{
+    public class UserOwnsPostQuery : IRequest<(bool Success, bool PostFound, bool UserOwnsPost)>
+    {
+        public Guid PostId { get; set; }
+        public string UserId { get; set; }
+    }
+}
