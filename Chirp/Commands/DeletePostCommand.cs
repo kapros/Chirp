03e8f12774b899ef2275d619511410367a846dc7
kapros <kamil.prosowski@gmail.com>
2021-01-02 20:35:04 +0100

    add command for deleting posts
    should be an event later on

---
+++ Chirp/Commands/DeletePostCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Commands
+{
+    public class DeletePostCommand : IRequest<bool>
+    {
+        public Guid PostId { get; set; }
+    }
+}
---
+++ Chirp/Handlers/CommandHandlers/DeletePostCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Chirp.Commands;
+using Chirp.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chirp.Handlers.CommandHandlers
+{
+    public class DeletePostCommandHandler : IRequestHandler<DeletePostCommand, bool>
+    {
+        private readonly IPostService _postService;
+
+        public DeletePostCommandHandler(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public async Task<bool> Handle(DeletePostCommand request, CancellationToken cancellationToken)
+        {
+            var deleted = await _postService.DeletePostAsync(request.PostId);
+            return deleted;
+        }
+    }
+}
